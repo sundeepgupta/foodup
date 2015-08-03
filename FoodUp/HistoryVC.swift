3fce328068e5e85367c7e5d493100ebfef57fb4c
Sundeep Gupta <sundeep@sundeepgupta.ca>
2015-08-03 14:57:40 -0400

    Passing data controller around.

--- FoodUp/HistoryVC.swift
+++ FoodUp/HistoryVC.swift
@@ -2,8 +2,22 @@ import UIKit
 
 class HistoryVC: UIViewController {
     @IBOutlet weak var textView: UITextView!
+    private var dataController: DataController!
+    
+    func setDataController(dataController: DataController) {
+        self.dataController = dataController;
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let meals = self.dataController.meals()
+        self.textView.text = meals.description
+    }
     
     @IBAction func done() {
         self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    
 }
--- FoodUp/TrackVC.swift
+++ FoodUp/TrackVC.swift
@@ -1,21 +1,45 @@
 import UIKit
 
 class TrackVC: UIViewController {
-
+    private var dataController: DataController!
+    
+    func setDataController(dataController: DataController) {
+        self.dataController = dataController;
+    }
+    
     @IBAction func ateSmallMeal() {
-        
+        self.processMeal(type: .Small)
     }
     
     @IBAction func ateMediumMeal() {
-        
+        self.processMeal(type: .Medium)
     }
     
     @IBAction func ateLargeMeal() {
-        
+        self.processMeal(type: .Large)
     }
 
     @IBAction func seeHistory() {
+        self.presentHistory()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let nc = segue.destinationViewController as! UINavigationController
+        if let historyVC = nc.viewControllers.first as? HistoryVC {
+            historyVC.setDataController(self.dataController)
+        }
+    }
+    
+    private func processMeal(type type: MealType) {
+        self.dataController.createMeal(type: type, time: NSDate())
+        self.presentHistory()
+        
+    }
+    
+    private func presentHistory() {
         self.performSegueWithIdentifier("trackToHistory", sender: nil)
     }
+    
+    
 }
 
