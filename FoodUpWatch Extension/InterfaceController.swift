adc9b6dd2d424e68b2dec7865d952c408e335829
Sundeep Gupta <sundeep@sundeepgupta.ca>
2015-08-03 22:29:12 -0400

    Clean up watch code.

--- FoodUpWatch Extension/InterfaceController.swift
+++ FoodUpWatch Extension/InterfaceController.swift
@@ -3,23 +3,34 @@ import WatchConnectivity
 import Foundation
 
 class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    let session = WCSession.defaultSession()
+    
+    override init() {
+        super.init()
+        
+        self.session.delegate = self
+        self.session.activateSession()
+    }
 
     @IBAction func ateSmallMeal() {
-        let session = WCSession.defaultSession()
-        session.delegate = self
-        session.activateSession()
-        let context = ["type": "Small"]
-        
-        do {
-            try session.updateApplicationContext(context)
-        } catch {
-            
-        }
+        self.createMeal("Small")
     }
 
     @IBAction func ateMediumMeal() {
+        self.createMeal("Medium")
     }
 
     @IBAction func ateLargeMeal() {
+        self.createMeal("Large")
+    }
+    
+
+    private func createMeal(type: String) {
+        let context = ["type": type]
+        do {
+            try session.updateApplicationContext(context)
+        } catch let error as NSError {
+            print("Error sending meal to phone: \(error)")
+        }
     }
 }
