00d7916329f21216d94dc5715e350fe09dc0df31
Sundeep Gupta <sundeep@sundeepgupta.ca>
2015-08-09 22:00:58 -0400

    Add id attribute to Meal.

--- FoodUp/Meal.swift
+++ FoodUp/Meal.swift
@@ -1,27 +1,31 @@
 import Foundation
 
 class Meal: NSObject, NSCoding {
+    let id: Int
     let type: MealType
     let time: NSDate
     
-    init(type: MealType, time: NSDate) {
+    init(type: MealType, time: NSDate, id: Int = -1) {
+        self.id = id
         self.type = type
         self.time = time
         super.init()
     }
     
     required convenience init?(coder decoder: NSCoder) {
+        let id = decoder.decodeIntegerForKey("id")
         let time = decoder.decodeObjectForKey("time") as! NSDate
         
         let typeValue = decoder.decodeObjectForKey("type") as! String
         if let typeEnum = MealType(rawValue: typeValue) {
-            self.init(type: typeEnum, time: time)
+            self.init(type: typeEnum, time: time, id: id)
         } else {
             fatalError("Meal type string from archive not recognized.")
         }
     }
     
     func encodeWithCoder(coder: NSCoder) {
+        coder.encodeInteger(self.id, forKey: "id")
         coder.encodeObject(self.type.rawValue, forKey: "type")
         coder.encodeObject(self.time, forKey: "time")
     }
